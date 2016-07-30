b7201584a3d48ac3d9622e1237c02592512d52ab
Chris Gough <chris.gough@environment.gov.au>
2016-07-30 17:34:52 +1000

    update EInvoice.sol so it actually works
    This is excessively simplistic contract is instantiated once for every ACK

--- etherium/EInvoiceACK.sol
+++ etherium/EInvoiceACK.sol
@@ -3,28 +3,46 @@ contract EInvoiceACK {
      * This contract is used by the ledger service to prove ACK happened
      *
      */
-    string payload_hash;
-    string gw_id;
-    string url;
-    int status;
-    string gw_datetime;
-    string post_datetime;
-    
-    function GWInit (string gw_id) {
-        gw_id = gw_id;
+    struct Ack {
+        string payload_hash;
+        string url;
+        int code;
+        uint gw_datetime;
+        uint post_datetime;
     }
-    
+    Ack public AckData;
+    uint post_datetime;
+      
     function postAck (
         string payload_hash,
         string url,
-        int status,
-        string gw_datetime,
-        string post_datetime) returns (address) {
-        url = url;
-        status = status;
-        gw_datetime =  gw_datetime;
-        post_datetime = post_datetime;
-        
-        return this;
+        int code,
+        uint gw_datetime) returns (address)
+        {
+        post_datetime = now;
+        AckData = Ack({
+            payload_hash: payload_hash,
+            url: url,
+            code: code,
+            gw_datetime: gw_datetime,
+            post_datetime: post_datetime
+        });        
+        return tx.origin;
+    }
+    
+    function getAckPayloadHash() returns (string) {
+        return AckData.payload_hash;
+    }
+    function getAckUrl() returns (string) {
+        return AckData.url;
+    }
+    function getAckstatus() returns (int) {
+        return AckData.code;
+    }
+    function getAckgw_datetime() returns (uint) {
+        return AckData.gw_datetime;
+    }
+    function getAckPostDateTime() returns (uint) {
+        return AckData.post_datetime;
     }
 }
