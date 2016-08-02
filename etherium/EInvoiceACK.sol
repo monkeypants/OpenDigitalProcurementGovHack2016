bac10c66dafb1dafc4f0ad6e83ec440e5983c795
Chris Gough <chris.gough@environment.gov.au>
2016-08-03 02:20:11 +1000

    update EInvoiceACK.sol with comments

--- etherium/EInvoiceACK.sol
+++ etherium/EInvoiceACK.sol
@@ -1,10 +1,24 @@
 contract EInvoiceACK {
     /* 
-     * This contract is used by the ledger service to prove ACK happened
+     * This contract is used to gazette the fact a message happened
      *
+     * Three types of information are recorded:
+     *  - what reference (reference_url. reference_hash)
+     *  - what message (taxonomy_url, taxonomy_code)
+     *  - when (sender_time, gazette_time)
+     *
+     * Do we need a URL for the type of hash, or possibly a hardcoded
+     * to an enumeration of hash types? What if it needs other
+     * parameters, PBKDF2 style?
+     *
+     * Do we need to record the poster's identity? Presumably
+     * "poster-pays" the blockchain costs, so we have to
+     * know who they are (???)
+     *
+     * TODO: change labels (as above) and add the taxonomy url.
      */
     struct Ack {
-        string payload_hash;
+        string payload_hash; 
         string url;
         int code;
         uint gw_datetime;
